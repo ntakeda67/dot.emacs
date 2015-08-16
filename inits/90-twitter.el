cbb4324fcce57ea9d1239b624224f616cf153ebf
natsuki.takeda <takeda@localhost.localdomain>
2015-08-16 12:43:56 +0900

    projectile

---
+++ inits/90-twitter.el
@@ -0,0 +1,7 @@
+(require 'twittering-mode)
+; Access Tokenの保存
+(setq twittering-use-master-password t)
+;アイコン表示
+(setq twittering-icon-mode t)
+;全てのアイコンを保存するか
+(setq twittering-icon-storage-limit t)
